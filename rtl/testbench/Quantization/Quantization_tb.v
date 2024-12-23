59988d721f699d40d59dc99a8b3bcccddd47b40d
zhy <Zhy_theta@outlook.com>
2024-12-23 23:01:55 +0800

    add readme

--- rtl/testbench/Quantization/Quantization_tb.v
+++ rtl/testbench/Quantization/Quantization_tb.v
@@ -1,13 +1,19 @@
 // Quantization_tb.v
-
+`include "../../Quantization.v"
 module Quantization_tb();
 
+reg 
+
+
 Quantization #(
-  .INPUT_INTEGER_WIDTH(),
-  .INPUT_DECIMAL_WIDTH(),
-  .OUTPUT_INTEGER_WIDTH(),
-  .OUTPUT_DECIMAL_WIDTH()
-)
+  .INPUT_INTEGER_WIDTH(18),
+  .INPUT_DECIMAL_WIDTH(16),
+  .OUTPUT_INTEGER_WIDTH(8),
+  .OUTPUT_DECIMAL_WIDTH(8)
+) DUT(
+  .in(in_data),
+  .out(out_data)
+);
 
 
 endmodule;
